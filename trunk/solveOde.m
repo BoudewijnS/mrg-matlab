8c4b8e51f99c6d3cc2f69c1faf1bd4cba31686ed
simon.danner <simon.danner@5117ea12-0ef2-11de-9106-f38757f67524>
2009-09-14 11:55:55 +0000

    -

--- trunk/solveOde.m
+++ trunk/solveOde.m
@@ -1,84 +1,185 @@
 function [t,Y,xs]=solveOde(dur,IC)
 
-    % parameters
-    % ----------------------------------------------------------------
-    l_n = 1.5e-4;        % node length [cm]
-    D = 20e-4;           % fiber diameter with myelin sheath [cm]
-    g_nap = 10;         % sodium channel conductivity [mS/cm^2]
-    g_naf = 3000;        %
-    g_k = 80;
+%     % parameters
+%     % ----------------------------------------------------------------
+%     l_n = 1.5e-4;        % node length [cm]
+%     D = 20e-4;           % fiber diameter with myelin sheath [cm]
+%     g_nap = 0.010;         % sodium channel conductivity [S/cm^2]
+%     g_naf = 3;        %
+%     g_k = 0.080;
+%     g_kf = 0;
+%     g_l = 0.007;           % leak channel conductivity [S/cm^2] 
+%     e_na = 50.0;
+%     e_k = -90.0;
+%     e_l	= -90.0;
+% 
+%     r = 70;           	%Ohm*cm
+%     rhoa = 7e5;          %Ohm*um
+%     c = 2;             % specific capacity [muF/cm^2]
+%     mycm=0.1 ;
+% 	mygm=.001;
+%     V_fem = 50;          % Voltage applied in FEM simulation [V]
+%     polarity = -1;       % -1/+1 ... neg./pos. active electrode
+%     % ----------------------------------------------------------------
+%     mysalength=3.0e-4; 
+% 	nodelength=1.0e-4;
+%     vrest = -80; %Initial Voltage
+%     % Parameters from neuron file with fiberD 14 (needs to be corrected)
+%     fiberD=14.0e-4;
+%     
+%     axonD=10.4e-4 ;
+%     nodeD=4.7e-4 ;
+%     mysaD=4.7e-4 ;
+%     flutD=10.4e-4 ;
+%     space_p1=0.002e-4;
+%     space_p2=0.004e-4;
+%     space_i=0.004e-4;
+%     deltax=1400e-4 ;
+%     nl = 140;
+%     
+%     flutlength=56e-4;
+%     interlength=(deltax-nodelength-(2*mysalength)-(2*flutlength))/6;
+%     
+%     c_mysa = c*mysaD*pi*mysalength*(mysaD/fiberD);
+%     c_flut = c*flutD*pi*flutlength*(flutD/fiberD);
+%     c_inter = c*axonD*pi*interlength*(axonD/fiberD);
+%     c_node = c*nodeD*pi*nodelength;
+%     
+%     c_mysa_m = mycm/nl*mysaD*pi*mysalength;
+%     c_flut_m = mycm/nl*flutD*pi*flutlength;
+%     c_inter_m = mycm/nl*axonD*pi*interlength;
+%     
+%     g_mysa_m = mygm/nl*mysaD*pi*mysalength;
+%     g_flut_m = mygm/nl*flutD*pi*flutlength;
+%     g_inter_m = mygm/nl*axonD*pi*interlength;
+%     
+% %     
+% %     g_mysa = 0.001.*mysaD./fiberD;
+% %     g_flut = 0.0001.*flutD./fiberD;
+% %     g_inter = 0.0001.*axonD./fiberD;
+%     
+%     g_mysa = 0.001*mysaD*pi*mysalength*(mysaD/fiberD);
+%     g_flut = 0.0001*flutD*pi*flutlength*(flutD/fiberD);
+%     g_inter = 0.0001*axonD*pi*interlength*(axonD/fiberD);
+%     g_nap = g_nap*nodeD*pi*nodelength;
+%     g_naf = g_naf*nodeD*pi*nodelength;
+%     g_k = g_k*nodeD*pi*nodelength;
+%     g_l = g_l*nodeD*pi*nodelength;
+%     
+%     
+%     r_mysa = r*4*mysalength/(mysaD^2*pi)*(1/(mysaD/fiberD)^2);
+%     r_flut = r*4*flutlength/(flutD^2*pi)*(1/(flutD/fiberD)^2);
+%     r_inter = r*4*interlength/(axonD^2*pi)*(1/(axonD/fiberD)^2);
+%     r_node = r*4*nodelength/(nodeD^2*pi);
+%     
+%     r_pn0=(r*0.01*nodelength)/(pi*((((nodeD/2)+space_p1)^2)-((nodeD/2)^2)));
+% 	r_pn1=(r*0.01*mysalength)/(pi*((((mysaD/2)+space_p1)^2)-((mysaD/2)^2)));
+% 	r_pn2=(r*0.01*flutlength)/(pi*((((flutD/2)+space_p2)^2)-((flutD/2)^2)));
+% 	r_px=(r*0.01*interlength)/(pi*((((axonD/2)+space_i)^2)-((axonD/2)^2)));
+    
+    vrest = -80;
+    fiberD=10.0;        %um
+    paralength1=3;      %um  
+    nodelength=1.0;
+    space_p1=0.002;  
+    space_p2=0.004;
+    space_i=0.004;
+    rhoa=0.7e9;         %Ohm*um
+    mycm=0.1;           %uF/cm2
+    mygm=0.001;         %S/cm2
+    c=2;                %uF/cm2
+
+    g_nap = 0.010;         % sodium channel conductivity [S/cm^2]
+    g_naf = 3;        %
+    g_k = 0.080;
     g_kf = 0;
-    g_l = 7;           % leak channel conductivity [mS/cm^2] 
+    g_l = 0.007;           % leak channel conductivity [S/cm^2] 
     e_na = 50.0;
     e_k = -90.0;
     e_l	= -90.0;
 
-    r = 70;           % specific resistivity 
-    c = 2;             % specific capacity [muF/cm^2]
-    rhoa=0.7e6;         %Ohm um
-    mycm=0.1 ;
-	mygm=1;
-    k = 1;               % 37?C  k=3^(0.1*T-3.7)
-    V_fem = 50;          % Voltage applied in FEM simulation [V]
-    polarity = -1;       % -1/+1 ... neg./pos. active electrode
-    % ----------------------------------------------------------------
-    mysalength=3.0e-4; 
-	nodelength=1.0e-4;
-    vrest = -80; %Initial Voltage
-    % Parameters from neuron file with fiberD 14 (needs to be corrected)
-    fiberD=14.0e-4;
+    g=0.690; 
+    axonD=6.9; 
+    nodeD=3.3; 
+    paraD1=3.3; 
+    paraD2=6.9; 
+    deltax=1150; 
+    paralength2=46; 
+    nl=120;
     
-    axonD=10.4e-4 ;
-    nodeD=4.7e-4 ;
-    mysaD=4.7e-4 ;
-    flutD=10.4e-4 ;
-    space_p1=0.002e-4;
-    space_p2=0.004e-4;
-    space_i=0.004e-4;
-    deltax=1400e-4 ;
-    nl = 140;
-    xg = mygm/(nl*2);   %g for membrane
-    xc = mycm/(nl*2);   %c for mambrane
-    flutlength=56e-4;
-    interlength=(deltax-nodelength-(2*mysalength)-(2*flutlength))/6;
+    mysalength = paralength1;
+    flutlength = paralength2;
     
-    c_mysa = c*mysaD*pi*mysalength;
-    c_flut = c*flutD*pi*flutlength;
-    c_inter = c*axonD*pi*interlength;
-    c_node = c*nodeD*pi*nodelength;
-    
-    c_mysa_m = mycm/nl*mysaD*pi*mysalength;
-    c_flut_m = mycm/nl*flutD*pi*flutlength;
-    c_inter_m = mycm/nl*axonD*pi*interlength;
-    
-    g_mysa_m = mygm/nl*mysaD*pi*mysalength;
-    g_flut_m = mygm/nl*flutD*pi*flutlength;
-    g_inter_m = mygm/nl*axonD*pi*interlength;
-    
-%     
-%     g_mysa = 0.001.*mysaD./fiberD;
-%     g_flut = 0.0001.*flutD./fiberD;
-%     g_inter = 0.0001.*axonD./fiberD;
-    
-    g_mysa = 1*mysaD*pi*mysalength;
-    g_flut = 0.1*flutD*pi*flutlength;
-    g_inter = 0.1*axonD*pi*interlength;
-    g_nap = g_nap*nodeD*pi*nodelength;
-    g_naf = g_naf*nodeD*pi*nodelength;
-    g_k = g_k*nodeD*pi*nodelength;
-    g_l = g_l*nodeD*pi*nodelength;
-    
-    
-    r_mysa = r*4*mysalength/(mysaD^2*pi);
-    r_flut = r*4*flutlength/(flutD^2*pi);
-    r_inter = r*4*interlength/(axonD^2*pi);
-    r_node = r*4*nodelength/(nodeD^2*pi);
-    
-    r_pn0=(r*nodelength)/(pi*((((nodeD/2)+space_p1)^2)-((nodeD/2)^2)));
-	r_pn1=(r*mysalength)/(pi*((((mysaD/2)+space_p1)^2)-((mysaD/2)^2)));
-	r_pn2=(r*flutlength)/(pi*((((flutD/2)+space_p2)^2)-((flutD/2)^2)));
-	r_px=(r*interlength)/(pi*((((axonD/2)+space_i)^2)-((axonD/2)^2)));
-    
+    Rpn0=(rhoa*.01)/(pi*((((nodeD/2)+space_p1)^2)-((nodeD/2)^2)));
+    Rpn1=(rhoa*.01)/(pi*((((paraD1/2)+space_p1)^2)-((paraD1/2)^2)));
+    Rpn2=(rhoa*.01)/(pi*((((paraD2/2)+space_p2)^2)-((paraD2/2)^2)));
+    Rpx=(rhoa*.01)/(pi*((((axonD/2)+space_i)^2)-((axonD/2)^2)));
+    interlength=(deltax-nodelength-(2*paralength1)-(2*paralength2))/6;
+
+    %periaxonal resistivity in GOhm
+    r_pn0=Rpn0*(nodelength/10000)/1e3;
+    r_pn1=Rpn1*(paralength1/10000)/1e3;
+    r_pn2=Rpn2*(paralength2/10000)/1e3;
+    r_px=Rpx*(interlength/10000)/1e3;
+
+    %surface area in cm2
+    sa_node = (pi*nodeD*nodelength)/1e8;
+    sa_mysa = (pi*fiberD*paralength1)/1e8;
+    sa_flut = (pi*fiberD*paralength2)/1e8;
+    sa_inter = (pi*fiberD*interlength)/1e8;
+
+    %resistiviy in Ohm*cm
+    r_node = rhoa/10000;
+    r_mysa = rhoa*(1/(paraD1/fiberD)^2)/10000;
+    r_flut = rhoa*(1/(paraD2/fiberD)^2)/10000;
+    r_inter = rhoa*(1/(axonD/fiberD)^2)/10000;
+
+    %resistiviy in GOhm
+    r_node = (4*(nodelength/10000)*r_node)/(pi*(nodeD/10000)^2)/1e9;
+    r_mysa = (4*(paralength1/10000)*r_mysa)/(pi*(fiberD/10000)^2)/1e9;
+    r_flut = (4*(paralength2/10000)*r_flut)/(pi*(fiberD/10000)^2)/1e9;
+    r_inter = (4*(interlength/10000)*r_inter)/(pi*(fiberD/10000)^2)/1e9;
+
+    %capacity in uF/cm2
+    c_node = c;
+    c_mysa = c*paraD1/fiberD;
+    c_flut = c*paraD2/fiberD;
+    c_inter = c*axonD/fiberD;
+
+    %capacity in pF
+    c_node = c_node*sa_node*1e6;
+    c_mysa = c_mysa*sa_mysa*1e6;
+    c_flut = c_flut*sa_flut*1e6;
+    c_inter = c_inter*sa_inter*1e6;
+
+    %passive conductance in S/cm^2
+    g_mysa = 0.001*paraD1/fiberD;
+    g_flut = 0.0001*paraD2/fiberD;
+    g_inter = 0.0001*axonD/fiberD;
+    %in nS
+    g_mysa = g_mysa*sa_mysa*1e9;
+    g_flut = g_flut*sa_flut*1e9;
+    g_inter = g_inter*sa_inter*1e9;
+
+    %membrane conductance in S/cm2
+    xg = 0.001/(nl*2); 
+    %in nS
+    g_mysa_m = xg*sa_mysa*1e9;
+    g_flut_m = xg*sa_flut*1e9;
+    g_inter_m = xg*sa_inter*1e9;
+
+    %membrane capacity in uF/cm2
+    xc = 0.1/(nl*2);
+    %in pF
+    c_mysa_m = xc*sa_mysa*1e6;
+    c_flut_m = xc*sa_flut*1e6;
+    c_inter_m = xc*sa_inter*1e6;
+
+    %nodal conductances
+    g_nap = g_nap*sa_node*1e9;
+    g_naf = g_naf*sa_node*1e9;
+    g_k = g_k*sa_node*1e9;
+    g_l = g_l*sa_node*1e9;
     
     
     %how many nodes to simulate
@@ -116,9 +217,9 @@ function [t,Y,xs]=solveOde(dur,IC)
     %Dummy Stimulusvoltage
     V_e = zeros(i_inter(6,2),1);
     V_stim = zeros(i_inter(6,2),1);
-    q = -1000;
-    xe = 5000e-4;
-    ye = 5000e-4;
+    q = 500000;
+    xe = 14000;
+    ye = 5000;
     V_stim(1:N_nodes) = electrode(q,xe,ye,((1:N_nodes)-1)*deltax,zeros(1,N_nodes));
     V_stim(i_mysa(1):i_mysa(2)) = electrode(q,xe,ye,((1:N_inter)-1)*deltax+mysalength/2,zeros(1,N_inter));
     V_stim(i_mysa(3):i_mysa(4)) = electrode(q,xe,ye,((1:N_inter))*deltax-mysalength/2,zeros(1,N_inter));
@@ -131,7 +232,7 @@ function [t,Y,xs]=solveOde(dur,IC)
     V_stim(i_inter(5,1):i_inter(5,2)) = electrode(q,xe,ye,((1:N_inter)-1)*deltax+mysalength+flutlength+9*interlength/2,zeros(1,N_inter));
     V_stim(i_inter(6,1):i_inter(6,2)) = electrode(q,xe,ye,((1:N_inter)-1)*deltax+mysalength+flutlength+11*interlength/2,zeros(1,N_inter));
     
-    clf();
+
     figure(2);
     plot(1:N_nodes,V_stim(1:N_nodes));
     hold off;
@@ -156,9 +257,10 @@ function [t,Y,xs]=solveOde(dur,IC)
     plot(t,V);
     hold off;
     figure(3);
-    plot(xs(:,4),((xs(:,1)-xs(:,2))./(r_node+r_mysa))./c_node,xs(:,4),((xs(:,1)-xs(:,3))./(r_node+r_mysa))./c_node);
+    %plot(xs(:,4),((xs(:,1)-xs(:,2))./(r_node+r_mysa))./c_node,xs(:,4),((xs(:,1)-xs(:,3))./(r_node+r_mysa))./c_node);
+    plot(t,Y(:,i_inter_b(3,1)+11));
     figure(4);
-    plot(t,Y(:,5));
+    plot(t,Y(:,11));
     
     % odeMcIntyr: calculates the first derivative of all parameters of the
     %             McIntyre nerve model
@@ -168,7 +270,7 @@ function [t,Y,xs]=solveOde(dur,IC)
                 %
                 V_e = V_stim;
                 %Y(11) = Y(11) -30;
-                %V_e(11) = -1e1;
+                %V_e(11) = -2;
             else
                 V_e = zeros(i_inter(6,2),1);
             end
@@ -194,23 +296,7 @@ function [t,Y,xs]=solveOde(dur,IC)
         inter(:,4) = Y(i_inter(4,1):i_inter(4,2));
         inter(:,5) = Y(i_inter(5,1):i_inter(5,2));
         inter(:,6) = Y(i_inter(6,1):i_inter(6,2));
-        
-%         mysa_l_b = Y(i_mysa_b(1):i_mysa_b(2))+V_e(i_mysa(1):i_mysa(2));
-%         mysa_r_b = Y(i_mysa_b(3):i_mysa_b(4))+V_e(i_mysa(3):i_mysa(4));
-%         flut_l_b = Y(i_flut_b(1):i_flut_b(2))+V_e(i_flut(1):i_flut(2));
-%         flut_r_b = Y(i_flut_b(3):i_flut_b(4))+V_e(i_flut(3):i_flut(4));
-%         inter_b(:,1) = Y(i_inter_b(1,1):i_inter_b(1,2))+ ...
-%             V_e(i_inter(1,1):i_inter(1,2));
-%         inter_b(:,2) = Y(i_inter_b(2,1):i_inter_b(2,2))+ ...
-%             V_e(i_inter(2,1):i_inter(2,2));
-%         inter_b(:,3) = Y(i_inter_b(3,1):i_inter_b(3,2))+ ...
-%             V_e(i_inter(3,1):i_inter(3,2));
-%         inter_b(:,4) = Y(i_inter_b(4,1):i_inter_b(4,2))+ ...
-%             V_e(i_inter(4,1):i_inter(4,2));
-%         inter_b(:,5) = Y(i_inter_b(5,1):i_inter_b(5,2))+ ...
-%             V_e(i_inter(5,1):i_inter(5,2));
-%         inter_b(:,6) = Y(i_inter_b(6,1):i_inter_b(6,2))+ ...
-%             V_e(i_inter(6,1):i_inter(6,2));  
+       
 
         mysa_l_b = Y(i_mysa_b(1):i_mysa_b(2));
         mysa_r_b = Y(i_mysa_b(3):i_mysa_b(4));
@@ -221,7 +307,13 @@ function [t,Y,xs]=solveOde(dur,IC)
         inter_b(:,3) = Y(i_inter_b(3,1):i_inter_b(3,2));
         inter_b(:,4) = Y(i_inter_b(4,1):i_inter_b(4,2));
         inter_b(:,5) = Y(i_inter_b(5,1):i_inter_b(5,2));
-        inter_b(:,6) = Y(i_inter_b(6,1):i_inter_b(6,2));  
+        inter_b(:,6) = Y(i_inter_b(6,1):i_inter_b(6,2)); 
+        
+        mysa_lv = mysa_l + mysa_l_b;
+        mysa_rv = mysa_r + mysa_r_b;
+        flut_lv = flut_l + flut_l_b;
+        flut_rv = flut_r + flut_r_b;
+        interv = inter+inter_b;
         
         xs=[xs;V_e(11),mysa_l_b(11),V_e(i_mysa(1)+11),t];
         
@@ -229,8 +321,8 @@ function [t,Y,xs]=solveOde(dur,IC)
         % what to do at first and last node??
         [Iax,dpara_m,dpara_h,dpara_p,dpara_s] = axnode(node,para_m,...
                                                 para_h,para_p,para_s);
-        dnode = cableEq(Iax,node,[node(1);mysa_l],...
-            [mysa_r;node(N_nodes)],V_e(i_node(1):i_node(2)),...
+        dnode = cableEq(Iax,node,[node(1);mysa_lv],...
+            [mysa_rv;node(N_nodes)],V_e(i_node(1):i_node(2)),...
             [V_e(1);V_e(i_mysa(1):i_mysa(2))],[V_e(i_mysa(3):i_mysa(4));V_e(N_nodes)],...
             r_node,r_mysa,r_mysa,c_node);
         
@@ -239,10 +331,10 @@ function [t,Y,xs]=solveOde(dur,IC)
         Imy_l = mysa(mysa_l);
         Imy_r = mysa(mysa_r);
         
-        dmysa_l = cableEq(Imy_l,mysa_l,node(1:N_inter),flut_l, ...
+        dmysa_l = cableEq(Imy_l,mysa_lv,node(1:N_inter),flut_lv, ...
                           V_e(i_mysa(1):i_mysa(2)),V_e(i_node(1):i_node(2)-1), ...
                           V_e(i_flut(1):i_flut(2)),r_mysa,r_node,r_flut,c_mysa);
-        dmysa_r = cableEq(Imy_r,mysa_r,node(2:N_nodes),flut_r, ...
+        dmysa_r = cableEq(Imy_r,mysa_rv,node(2:N_nodes),flut_rv, ...
                           V_e(i_mysa(3):i_mysa(4)),V_e(i_node(1)+1:i_node(2)), ...
                           V_e(i_flut(3):i_flut(4)),r_mysa,r_node,r_flut,c_mysa);
         
@@ -268,10 +360,10 @@ function [t,Y,xs]=solveOde(dur,IC)
         [Ifl_rp, dpara_n_r] = flutPotassium(flut_r,para_n_r);
         Ifl_r = Ifl_r + Ifl_rp;
         
-        dflut_l = cableEq(Ifl_l,flut_l,mysa_l,inter(:,1), ...
+        dflut_l = cableEq(Ifl_l,flut_lv,mysa_lv,interv(:,1), ...
                           V_e(i_flut(1):i_flut(2)),V_e(i_mysa(1):i_mysa(2)),V_e(i_inter(1,1):i_inter(1,2)), ...
                           r_flut,r_mysa,r_inter,c_flut);
-        dflut_r = cableEq(Ifl_r,flut_r,mysa_r,inter(:,6), ...
+        dflut_r = cableEq(Ifl_r,flut_rv,mysa_rv,interv(:,6), ...
                           V_e(i_flut(3):i_flut(4)),V_e(i_mysa(3):i_mysa(4)),V_e(i_inter(6,1):i_inter(6,2)), ...
                           r_flut,r_mysa,r_inter,c_flut);
         
@@ -288,7 +380,7 @@ function [t,Y,xs]=solveOde(dur,IC)
         
         %internode currents
         Iin = internodes(inter);
-        inter2 = [flut_l,inter,flut_r];
+        inter2 = [flut_lv,interv,flut_rv];
         inter2b = [flut_l_b,inter_b,flut_r_b];
         inter2e = [V_e(i_flut(1):i_flut(2)),...
             V_e(i_inter(1,1):i_inter(1,2)),...
@@ -342,8 +434,8 @@ function [t,Y,xs]=solveOde(dur,IC)
     % extracellular: extracellular currents similar to the Neuron function
     %                with the same name
     function dV = extracellular(I,V,V1,V2,Ra,Ra1,Ra2,C,Imem,Cmem)
-       dV = (-I+Imem + (V1-V)./(Ra1./2+Ra./2) + (V2-V)./(Ra2./2+Ra./2))./C;
-       
+       %dV = (-I + Imem+ (V1-V)./(Ra1./2+Ra./2) + (V2-V)./(Ra2./2+Ra./2))./C;
+       dV = -I./C + Imem./Cmem + ((V1-V)./(Ra1./2+Ra./2) + (V2-V)./(Ra2./2+Ra./2))./(C+Cmem);
     end
    
     
